f102ecf95421a0815d2ee809d743c34291f4308f
Young Hwang <ykcul02@gmail.com>
2022-02-01 23:03:45 +0900

    feat: tag hidden

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const loginForm = document.querySelector('#login-form')
 const loginInput = document.querySelector('#login-form input')
-const link = document.querySelector('a')
+const loginUsername = document.querySelector('#greeting')
+
+const HIDDEN_CLASS_NAME = 'hidden'
 
 function onLoginSubmit(event) {
   event.preventDefault()
   const username = loginInput.value
-  console.log(username)
-  loginForm.classList.add('hidden')
+  loginForm.classList.add(HIDDEN_CLASS_NAME)
+  loginUsername.innerText = 'Hello ' + username
+  loginUsername.classList.remove(HIDDEN_CLASS_NAME)
 }
 
 loginForm.addEventListener('submit', onLoginSubmit)
