487a6fa26a0f520af656b1a1ded72793f328ec3d
Young Hwang <ykcul02@gmail.com>
2022-02-03 17:06:02 +0900

    feat: localstorage test

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ const loginInput = document.querySelector('#login-form input')
 const loginUsername = document.querySelector('#greeting')
 
 const HIDDEN_CLASS_NAME = 'hidden'
+const USERNAME_KEY = 'username'
 
 function onLoginSubmit(event) {
   event.preventDefault()
   const username = loginInput.value
+  localStorage.setItem(USERNAME_KEY, username)
   loginForm.classList.add(HIDDEN_CLASS_NAME)
-  loginUsername.innerText = `Hello  ${username}`
-  loginUsername.classList.remove(HIDDEN_CLASS_NAME)
+  paintGreeting(username)
 }
 
-loginForm.addEventListener('submit', onLoginSubmit)
+function paintGreeting(username) {
+  loginUsername.classList.remove(HIDDEN_CLASS_NAME)
+  loginUsername.innerText = `Hello  ${username}`
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASS_NAME)
+  loginForm.addEventListener('submit', onLoginSubmit)
+} else {
+  paintGreeting(savedUsername)
+}
