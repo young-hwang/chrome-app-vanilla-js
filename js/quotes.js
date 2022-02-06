e092488d36187208fe9abea3055e2a3e8f285fca
Young Hwang <ykcul02@gmail.com>
2022-02-06 18:53:10 +0900

    feat: add random quote

--- js/quotes.js
+++ js/quotes.js
@@ -42,5 +42,9 @@ const quotes = [
   },
 ]
 
-const quote = document.querySelector('#quotes span:first-child')
-const author = document.querySelector('#quotes span:last-child')
+const quote = document.querySelector('#quote span:first-child')
+const author = document.querySelector('#quote span:last-child')
+
+const todayQuote = quotes[Math.floor(Math.random() * quotes.length)]
+quote.innerText = todayQuote.quote
+author.innerText = todayQuote.author
